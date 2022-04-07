4bc1c22da7270bff8538b67cb548871a33a647b6
Yorvin Luna Wisky <yorvin0506@gmail.com>
2022-04-06 23:29:39 -0400

    API added.

---
+++ script.js
@@ -0,0 +1,44 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const TwitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from aqpiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        
+    authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 100) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+}
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        //Catch error here
+    }
+}
+
+// On Load
+getQuotes();
